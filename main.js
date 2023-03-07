efcf92d64a3c257c130157f4115e13ae212fa06e
priyanshadwani5 <127004056+priyanshadwani5@users.noreply.github.com>
2023-03-07 12:32:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $ ("input").on ('keyup' , function (e) {
+        if (e.key == 'Enter ' || e.key == 13) {
+            let q= $('input').val();
+            window.open('http://google.com/search?q='\+q);
+
+        }
+    })
+})
